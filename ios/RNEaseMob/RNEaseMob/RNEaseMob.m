5be3d2684cdb0d5df7c9ef4b0478c7fb449d7f0e
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-06-06 15:49:13 +0800

    添加基本文件。

---
+++ ios/RNEaseMob/RNEaseMob/RNEaseMob.m
@@ -0,0 +1,13 @@
+//
+//  RNEaseMob.m
+//  RNEaseMob
+//
+//  Created by Xiaosong Gao on 2018/6/6.
+//  Copyright © 2018年 Hecom. All rights reserved.
+//
+
+#import "RNEaseMob.h"
+
+@implementation RNEaseMob
+
+@end
