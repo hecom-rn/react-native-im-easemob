ae59575b34211d5fba2cf97f3217c3ea6145142f
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-07-11 18:01:13 +0800

    Fix：初始化方法调用错误。

--- src/module/Client.js
+++ src/module/Client.js
@@ -4,7 +4,7 @@ import NativeUtil from '../native';
 const Client = NativeModules.Client;
 const isIos = Platform.OS === 'ios';
 
-export const initWithAppKey = (appKey, options = {}) => NativeUtil(RNEaseMobModule.init, {appKey, ...options});
+export const initWithAppKey = (appKey, options = {}) => NativeUtil(Client.init, {appKey, ...options});
 
 export const login = (username, password) => isIos ?
     NativeUtil(Client.login, {username, password}) : Client.login(username, password);
