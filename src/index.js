117e658dced36367559ebfa1f1d9bfe94507ffdb
zzg <zhangzhiguo@hecom.cn>
2018-07-18 18:05:52 +0800

    修改导出文件

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import * as EventEmitter from './eventemitter';
 import * as ChatManager from './util/ChatUtil';
 import * as GroupManager from './util/GroupUtil';
 import * as Client from './native/Client';
-import EventType  from './constant/EventType';
 import * as IMConstant  from './constant/IMConstant';
-import ListenerType  from './constant/ListenerType';
+import * as  ChatManagerDelegate from './native/ChatManagerDelegate';
 
 export {
     EventEmitter,
     ChatManager,
     GroupManager,
     Client,
-    EventType,
     IMConstant,
-    ListenerType,
+    ChatManagerDelegate,
 };
