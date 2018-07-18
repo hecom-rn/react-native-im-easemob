ec829431d34841e6ec3746b245ab3044ca5db655
zzg <zhangzhiguo@hecom.cn>
2018-07-18 09:47:08 +0800

    修改model转字典方式。

--- ios/util/NSObject+Util.m
+++ ios/util/NSObject+Util.m
@@ -8,6 +8,7 @@
 
 #import "NSObject+Util.h"
 #import <objc/runtime.h>
+#import <MJExtension/MJExtension.h>
 
 @implementation NSObject (Util)
 
@@ -35,71 +36,7 @@
 }
 
 - (NSDictionary *)objectToDictionary {
-    NSArray *blacklist = @[@"coreObject",@"coreBody",@"impl"];//解析不出来的字段
-    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-    unsigned int count;
-    objc_property_t *propertyList = class_copyPropertyList([self class], &count);
-    for (int i = 0; i < count; i++) {
-        objc_property_t property = propertyList[i];
-        const char *cName = property_getName(property);
-        NSString *name = [NSString stringWithUTF8String:cName];
-        if ([blacklist containsObject:name]) {
-            continue;
-        }
-        NSObject *value = [self valueForKey:name];//valueForKey返回的数字和字符串都是对象
-        if ([value isKindOfClass:[NSString class]] || [value isKindOfClass:[NSNumber class]]) {
-            //string , bool, int ,NSinteger
-            [dic setObject:value forKey:name];
-        } else if ([value isKindOfClass:[NSArray class]] || [value isKindOfClass:[NSDictionary class]]) {
-            //字典或字典
-            [dic setObject:[self arrayOrDicWithObject:(NSArray*)value] forKey:name];
-        } else if (value == nil) {
-            [dic setObject:[NSNull null] forKey:name];
-        } else {
-            //model
-            [dic setObject:[value objectToDictionary] forKey:name];
-        }
-    }
-    return [dic copy];
-}
-
-- (id)arrayOrDicWithObject:(id)origin {
-    if ([origin isKindOfClass:[NSArray class]]) {
-        //数组
-        NSMutableArray *array = [NSMutableArray array];
-        for (NSObject *object in origin) {
-            if ([object isKindOfClass:[NSString class]] || [object isKindOfClass:[NSNumber class]]) {
-                //string , bool, int ,NSinteger
-                [array addObject:object];
-            } else if ([object isKindOfClass:[NSArray class]] || [object isKindOfClass:[NSDictionary class]]) {
-                //数组或字典
-                [array addObject:[self arrayOrDicWithObject:(NSArray *)object]];
-            } else {
-                //model
-                [array addObject:[object objectToDictionary]];
-            }
-        }
-        return [array copy];
-    } else if ([origin isKindOfClass:[NSDictionary class]]) {
-        //字典
-        NSDictionary *originDic = (NSDictionary *)origin;
-        NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-        for (NSString *key in originDic.allKeys) {
-            id object = [originDic objectForKey:key];
-            if ([object isKindOfClass:[NSString class]] || [object isKindOfClass:[NSNumber class]]) {
-                //string , bool, int ,NSinteger
-                [dic setObject:object forKey:key];
-            } else if ([object isKindOfClass:[NSArray class]] || [object isKindOfClass:[NSDictionary class]]) {
-                //数组或字典
-                [dic setObject:[self arrayOrDicWithObject:object] forKey:key];
-            } else {
-                //model
-                [dic setObject:[object objectToDictionary] forKey:key];
-            }
-        }
-        return [dic copy];
-    }
-    return [NSNull null];
+    return [self mj_keyValuesWithIgnoredKeys:@[@"coreObject",@"impl",@"coreBody"]];
 }
 
 @end
