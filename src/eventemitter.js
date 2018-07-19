c4d1087988743bbdb4f95f00e52d1791e17cda56
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-07-19 09:52:19 +0800

    去除注册事件处理器。

--- src/eventemitter.js
+++ src/eventemitter.js
@@ -1,25 +1,9 @@
-import { NativeModules, NativeEventEmitter, DeviceEventEmitter, Platform } from 'react-native';
-import guid from './guid';
+import { NativeModules, Platform } from 'react-native';
 
 const isIOS = Platform.OS === 'ios';
 const RNEaseMobModule = NativeModules.Client;
-const event = isIOS ? new NativeEventEmitter(RNEaseMobModule) : DeviceEventEmitter;
-const handlers = {};
 
 // Android Only
 export function notifyJSDidLoad() {
     return !isIOS && RNEaseMobModule.notifyJSDidLoad();
-}
-
-export function registerEventHandler(func) {
-    const handlerId = guid();
-    handlers[handlerId] = event.addListener('RNEaseMob', func);
-    return handlerId;
-}
-
-export function unregisterEventHandler(handlerId) {
-    if (handlers && handlers[handlerId]) {
-        handlers[handlerId].remove();
-        delete handlers[handlerId];
-    }
 }
