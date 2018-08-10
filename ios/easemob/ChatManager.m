a944219e6d75c6b4536bda18193cc9581639d87e
zzg <zhangzhiguo@hecom.cn>
2018-08-10 17:53:39 +0800

    修改插入消息的状态。

--- ios/easemob/ChatManager.m
+++ ios/easemob/ChatManager.m
@@ -142,6 +142,7 @@ RCT_EXPORT_METHOD(sendMessage:(NSString *)params
         //插入消息
         message.timestamp = timestamp;
         message.localTime = localTime;
+        message.status = EMMessageStatusSucceed;
         EMConversationType type = [[allParams objectForKey:@"chatType"] intValue];
         EMConversation *conversation = [[EMClient sharedClient].chatManager getConversation:conversationId type:type createIfNotExist:NO];
         [conversation insertMessage:message error:nil];
