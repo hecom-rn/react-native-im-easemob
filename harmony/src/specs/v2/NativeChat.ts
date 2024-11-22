8c6e4b8b1846fc479e2dde97b0dca7566cca0bd0
田学军 <tianxuejun@hecom.cn>
2024-11-22 13:43:56 +0800

    feat(环信适配): 鸿蒙适配-环信适配
    --story=1013383 --user=田学军 【移动端】鸿蒙适配-环信适配 https://www.tapd.cn/23787791/s/1986829

---
+++ harmony/src/specs/v2/NativeChat.ts
@@ -0,0 +1,11 @@
+// NativeChat.ts
+import type {TurboModule} from 'react-native/Libraries/TurboModule/RCTExport';
+import {TurboModuleRegistry} from 'react-native';
+
+export interface Spec extends TurboModule {
+  add(a: number, b: number): Promise<number>;
+}
+
+export default TurboModuleRegistry.get<Spec>(
+  'RTNChat',
+) as Spec | null;
